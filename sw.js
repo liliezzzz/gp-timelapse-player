1d703b238cd138c0d9d7a4163f1f3c16b1c26e1d
liliezzzz <92837278+liliezzzz@users.noreply.github.com>
2023-12-31 12:35:52 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cachePrefix = 'gp-player-'
-const cacheVersion = 'v10'
+const cacheVersion = 'v13'
 const cacheName = `${cachePrefix}${cacheVersion}`
 
 const PATHNAME = '/gp-timelapse-player'
